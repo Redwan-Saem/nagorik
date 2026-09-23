ba90d364caaa74b039a8ace7522f96de4ec58b5a
Rajiya179 <rajiyatisha98@gmail.com>
2026-09-24 00:56:36 +0600

    T-021.4: Build registratrion & Login UI using html

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
--- obj/Debug/net10.0/Nagorik.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Nagorik.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nagorik.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f968f8ce839f96219d439cc10ac1a18b633d62f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3dbe9ccc280b282ea69f74608f4bdd90188036")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nagorik.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nagorik.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
